349c684174d51bf688d4f233055a8cecc8768300
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-04-06 17:15:18 +0600

    Product Screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to Electroshop</h1>
-    </>
-  );
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
