bb063f99ee06d4f92c1598f31d6b1fa196f2028b
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-04-07 03:03:08 +0600

    Add Environment Vars

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
+const dotenv = require('dotenv')
 const express = require('express')
 const products = require('./data/products')
+
+dotenv.config()
 
 const app = express()
 
@@ -16,6 +19,8 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, () => {
-  console.log('App listening on port 5000!')
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`)
 })
