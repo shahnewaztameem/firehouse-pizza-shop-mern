be793a5b5e5ed6ea6dc8e195bfc1cb1a55c24291
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-04-07 02:51:22 +0600

    Fetched Data from server

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import axios from 'axios'
 import Rating from '../components/Rating'
-import products from '../products'
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id)
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [match])
 
   return (
     <>
       <Link className='btn btn-dark my-3' to='/'>
-        <i class='fas fa-chevron-left'></i> Go Back
+        <i className='fas fa-chevron-left'></i> Go Back
       </Link>
       <Row>
         <Col md={6}>
