e9eb7c91e611cb9d48e1d741ecfd9cad7f7bbf1f
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-04-08 01:22:31 +0600

    Establish DB conncection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+
+//db connection
+connectDB()
 
 const app = express()
 
