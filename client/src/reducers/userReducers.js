e89e89459f4182ee350a4204dcc97418810f29b5
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-09 17:20:01 +0600

    Impl: User sign up

--- client/src/reducers/userReducers.js
+++ client/src/reducers/userReducers.js
@@ -3,6 +3,9 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
 } from '../constants/userConstants'
 
 export const userLoginReducer = (state = {}, action) => {
@@ -16,6 +19,19 @@ export const userLoginReducer = (state = {}, action) => {
 
     case USER_LOGOUT:
       return {}
+    default:
+      return state
+  }
+}
+
+export const userRegisterReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true }
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, userInfo: action.payload }
+    case USER_REGISTER_FAIL:
+      return { loading: false, error: action.payload }
     default:
       return state
   }
