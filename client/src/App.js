ff706ebd10630faf686343800cfb8c1dc610b016
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-11 18:32:31 +0600

    Feat: Payment Screen, shipping address Screen, Checkout Address added

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
 
 const App = () => {
   return (
@@ -17,9 +19,11 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
+          <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/payment' component={PaymentScreen} />
           <Route path='/login' component={LoginScreen} />
-          <Route path='/register' component={RegisterScreen } />
-          <Route path='/profile' component={ProfileScreen } />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/' component={HomeScreen} exact />
--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -1,5 +1,10 @@
 import axios from 'axios'
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD,
+} from '../constants/cartConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`)
@@ -23,3 +28,16 @@ export const removeFromCart = (id) => async (dispatch, getState) => {
   dispatch({ type: CART_REMOVE_ITEM, payload: id })
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
+
+export const saveShippingAddress = (data) => async (dispatch) => {
+  dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data })
+
+  localStorage.setItem('shippingAddress', JSON.stringify(data))
+}
+
+export const savePaymentMethod = (data) => async (dispatch) => {
+  dispatch({ type: CART_SAVE_PAYMENT_METHOD, payload: data })
+
+  localStorage.setItem('paymentMethod', JSON.stringify(data))
+}
+
