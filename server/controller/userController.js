88435fd09f38581f8ba7fc48abb18713d095ca5f
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-15 16:17:40 +0600

    Impl: user delete added

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -111,4 +111,20 @@ const getUsers = asyncHandler(async (req, res) => {
   res.json(users)
 })
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers }
+//@desc       Delete User
+//@route      GET /api/users/:id
+//@access     Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+  if(user) {
+    await user.remove()
+    res.json({message: 'User deleted successfully!'})
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+  
+})
+
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,6 +6,7 @@ import {
   getUserProfile,
   updateUserProfile,
   getUsers,
+  deleteUser,
 } from '../controller/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -15,5 +16,7 @@ router
   .route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile)
+
+router.route('/:id').delete(protect, admin, deleteUser)
 
 export default router
