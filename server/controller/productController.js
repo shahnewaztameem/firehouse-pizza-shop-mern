7d6615c70eccca62915763f18e324c4bcebf02d4
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-15 19:49:58 +0600

    Impl: delete product api endpoint

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -23,4 +23,19 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 })
 
-export { getProducts, getProductById }
+//@desc       Delete  a product
+//@route      DELETE /api/products/:id
+//@access     Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    await product.remove()
+    res.json({ messsage: 'Product Deleted Successfully' })
+  } else {
+    res.status(404)
+    throw new Error('Product Not Found!')
+  }
+})
+
+export { getProducts, getProductById, deleteProduct }
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,6 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import { getProducts, getProductById } from '../controller/productController.js'
+import {
+  getProducts,
+  getProductById,
+  deleteProduct,
+} from '../controller/productController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 //@desc       Fetch All products
 //@route      GET /api/products
 //@access     Public
@@ -9,6 +14,6 @@ router.route('/').get(getProducts)
 //@desc       Fetch a single products
 //@route      GET /api/products/:id
 //@access     Public
-router.route('/:id').get(getProductById)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
 
 export default router
