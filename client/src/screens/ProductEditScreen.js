04ce084f364314c096e0a8331fff0fb8906ea24e
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-17 00:02:22 +0600

    Impl: Upload image from frontend

--- client/src/screens/ProductEditScreen.js
+++ client/src/screens/ProductEditScreen.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Form, Button, Spinner } from 'react-bootstrap'
@@ -18,6 +19,7 @@ const ProductEditScreen = ({ match, history }) => {
   const [category, setCategory] = useState('')
   const [countInStock, setCountInStock] = useState(0)
   const [description, setDescription] = useState('')
+  const [uploading, setUploading] = useState(false)
 
   const dispatch = useDispatch()
 
@@ -67,6 +69,29 @@ const ProductEditScreen = ({ match, history }) => {
     )
   }
 
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0]
+    const formData = new FormData()
+    formData.append('image', file)
+    setUploading(true)
+
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      }
+      const { data } = await axios.post('/api/upload', formData, config)
+
+      setImage(data)
+      setUploading(false)
+
+    } catch (error) {
+      console.error(error)
+      setUploading(false)
+    }
+  }
+
   return (
     <>
       <Link to='/admin/productlist' className='btn btn-light my-3'>
@@ -76,7 +101,7 @@ const ProductEditScreen = ({ match, history }) => {
       <FormContainer>
         <h1>Edit Product Details</h1>
         {/*loadingUpdate && <Loader />*/}
-        {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>} 
+        {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
 
         {loading ? (
           <Loader />
@@ -112,6 +137,13 @@ const ProductEditScreen = ({ match, history }) => {
                 value={image}
                 onChange={(e) => setImage(e.target.value)}
               ></Form.Control>
+              <Form.File
+                id='image-file'
+                label='Choose File'
+                custom
+                onChange={uploadFileHandler}
+              ></Form.File>
+              {uploading && <Spinner size='sm' animation='border' />}
             </Form.Group>
 
             <Form.Group controlId='brand'>
