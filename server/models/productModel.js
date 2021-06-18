9a32e2a3b81d335aa580d1fdd9260aa0256f8199
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-18 20:15:36 +0600

    Impl: product review endpoint

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -5,7 +5,13 @@ const reviewSchema = mongoose.Schema(
     name: { type: String, required: true },
     rating: { type: Number, required: true },
     comment: { type: String, required: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
   },
+
   {
     timestamps: true,
   }
@@ -38,7 +44,7 @@ const productSchema = mongoose.Schema(
       type: String,
       required: true,
     },
-    reviwes: [reviewSchema],
+    reviews: [reviewSchema],
     rating: {
       type: Number,
       required: true,
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -6,10 +6,12 @@ import {
   deleteProduct,
   updateProduct,
   createProduct,
+  createProductReview,
 } from '../controller/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
 router
   .route('/:id')
   .get(getProductById)
