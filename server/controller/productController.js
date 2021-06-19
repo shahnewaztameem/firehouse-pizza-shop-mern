9aa216fbee78c6fcb372df7a640ebd1fe77e37e9
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-19 20:38:19 +0600

    Impl: get top 3 products

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -139,6 +139,14 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 })
 
+//@desc       Get Top rated products
+//@route      GET /api/products/top
+//@access     Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3)
+
+  res.json(products)
+})
 export {
   getProducts,
   getProductById,
@@ -146,4 +154,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 }
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -7,11 +7,13 @@ import {
   updateProduct,
   createProduct,
   createProductReview,
+  getTopProducts,
 } from '../controller/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
 router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTopProducts)
 router
   .route('/:id')
   .get(getProductById)
