1236653b4b0bba4d434f0a98633c698767fcbe45
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-20 01:36:30 +0600

    Fix: typo

--- client/src/components/SearchBox.js
+++ client/src/components/SearchBox.js
@@ -20,7 +20,7 @@ const SearchBox = ({ history }) => {
           type='text'
           name='q'
           onChange={(e) => setKeyword(e.target.value)}
-          placeHolder='Search Product'
+          placeholder='Search Product'
           className='mr-sm-2 ml-sm-5'
         ></Form.Control>
         <Button type='submit' variant='outline-success' className='p-2'>
