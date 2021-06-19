53eb4100a1965b7e36c65c5ca966ddff108bf162
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-19 20:34:43 +0600

    Impl: Pagination from backend

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -5,6 +5,9 @@ import Product from '../models/productModel.js'
 //@route      GET /api/products
 //@access     Public
 const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = 10
+  const page = Number(req.query.pageNumber) || 1
+
   const keyword = req.query.keyword
     ? {
         name: {
@@ -13,8 +16,13 @@ const getProducts = asyncHandler(async (req, res) => {
         },
       }
     : {}
-  const products = await Product.find({...keyword})
-  res.json(products)
+
+  const count = await Product.countDocuments({ ...keyword })
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1))
+
+  res.json({ products, page, pages: Math.ceil(count / pageSize) })
 })
 
 //@desc       Fetch a single products
