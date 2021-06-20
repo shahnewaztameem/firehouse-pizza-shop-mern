1acf9fc55088b20eea2b5fde249e6f851e0f64a5
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-21 02:00:43 +0600

    Add: favicon, fix title

--- client/src/components/Meta.js
+++ client/src/components/Meta.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-const Meta = ({title, description, keywords}) => {
+const Meta = ({ title, description, keywords }) => {
   return (
     <Helmet>
       <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta name="keywords" content={keywords} />
+      <meta name='description' content={description} />
+      <meta name='keywords' content={keywords} />
     </Helmet>
   )
 }
 
 Meta.defaultProps = {
-  title: 'Welcome to  Electroshop',
-  keywords: 'we sell best products',
-  description: 'we sell best products online, electronics, buy electronics',
+  title: 'Firehouse Pizza Shop',
+  keywords: 'we sell best pizzas, burger online',
+  description: 'we sell best best pizzas, burger online',
 }
 export default Meta
