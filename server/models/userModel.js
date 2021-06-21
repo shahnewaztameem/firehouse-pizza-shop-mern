0fd39ae62c3581c194c4a8baf4b427d75b835886
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-21 23:52:59 +0600

    Fix: mongoose custom error message

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,16 +5,16 @@ const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      required: [true, 'Name is required'],
     },
     email: {
       type: String,
-      required: true,
+      required: [true, 'Email is required'],
       unique: true,
     },
     password: {
       type: String,
-      required: true,
+      required: [true, 'Password is required'],
     },
     isAdmin: {
       type: Boolean,
