828b3c580bfb82880f347a16758d7d50fce8966c
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-27 20:28:48 +0600

    Fix server issue

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import dotenv from 'dotenv'
 import path from 'path'
 import morgan from 'morgan'
 import express from 'express'
-import helmet from 'helmet'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
@@ -18,16 +17,10 @@ connectDB()
 const app = express()
 
 app.use(express.json())
-app.disable('x-powered-by')
-app.use(helmet())
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
-
-app.get('/', (req, res) => {
-  res.send('Hello ')
-})
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -41,6 +34,18 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve()
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('Hello')
+  })
+}
 
 // 404 routing error handler
 app.use(notFound)
